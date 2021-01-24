b5b1c38c4e48d7e2e66dd6437119f081441ddd06
CateRios <caterinarios@gmail.com>
2021-01-24 23:58:04 +0000

    minor changes

--- src/views/Book.vue
+++ src/views/Book.vue
@@ -1,192 +1,162 @@
 <template>
-
   <div>
-
-    
-
     <!-- Rooms -->
     <v-container>
-      <RoomCard v-bind:room-data="roomData"/>
+      <RoomCard v-bind:room-data="roomData" />
     </v-container>
 
- <v-container fluid fill-height class="container">
-    <v-layout align-center justify-center>
-      <v-flex xs12 sm8 md4>
-        <!-- Card -->
-        <v-card class="elevation-5 ma-5 pa-5" color="rgb(255, 255, 255, 0.825)">
- <!-- Title -->
-          <v-card-title class="justify-center accent--text title">Book Form</v-card-title>
-
-          <!-- Form -->
-          <v-card-text class="pa-3">
-
-            <v-form ref="form"
-                    v-model="valid"
-                    lazy-validation
-                    @submit.prevent="sendEmail"
+    <v-container fluid fill-height class="container">
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md4>
+          <!-- Card -->
+          <v-card
+            class="elevation-5 ma-5 pa-5"
+            color="rgb(255, 255, 255, 0.825)"
+          >
+            <!-- Title -->
+            <v-card-title class="justify-center accent--text title"
+              >Book Form</v-card-title
             >
 
-              <!-- User field -->
-              <v-text-field
+            <!-- Form -->
+            <v-card-text class="pa-3">
+              <v-form
+                ref="form"
+                v-model="valid"
+                lazy-validation
+                @submit.prevent="sendEmail"
+              >
+                <!-- User field -->
+                <v-text-field
                   label="First Name"
                   type="text"
-                  color="accent" 
-                  :rules="userRules"
+                  color="accent"
                   required
-              />
+                />
 
-		<v-text-field
+                <v-text-field
                   label="Second Name"
                   type="text"
-                  color="accent" 
-                  :rules="userRules"
+                  color="accent"
                   required
-              />
+                />
 
-		<v-text-field
+                <v-text-field
                   label="Phone Number"
                   type="text"
-                  color="accent" 
-                  :rules="userRules"
+                  color="accent"
                   required
-              />
+                />
 
-		<v-text-field
+                <v-text-field
                   label="Email"
                   type="text"
                   color="accent"
                   v-model="email"
-                  :rules="userRules"
                   required
-              />
+                />
 
-		<v-text-field
+                <v-text-field
                   label="Company"
                   type="text"
-                  color="accent" 
-                  :rules="userRules"
+                  color="accent"
                   required
-              />
+                />
 
-		<v-text-field
+                <v-text-field
                   label="Credit Card Number"
                   type="text"
-                  color="accent" 
-                  :rules="userRules"
+                  color="accent"
                   required
-              />
+                />
 
-		<v-text-field
+                <v-text-field
                   label="Titular Name"
                   type="text"
-                  color="accent" 
-                  :rules="userRules"
+                  color="accent"
                   required
-              />
+                />
 
-		<v-text-field
+                <v-text-field
                   label="Expiration Date"
                   type="text"
-                  color="accent" 
-                  :rules="userRules"
+                  color="accent"
                   required
-              />
+                />
 
-		<v-text-field
+                <v-text-field
                   label="Comments"
                   type="text"
-                  color="accent" 
-                  :rules="userRules"
+                  color="accent"
                   required
-              />
+                />
 
-            <v-btn block color="accent" type="submit">Book</v-btn>
-         
-            </v-form>
-
-          </v-card-text>
-
- </v-card>
-
-</v-flex>
-    </v-layout>
-  </v-container>
-
+                <v-btn block color="accent" type="submit">Book</v-btn>
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
-
-
 </template>
 
 <script>
-
 import RoomCard from "@/components/Room-card";
-import emailjs from 'emailjs-com';
-import {getSelectedRoom} from "/server/functions/roomFunctions";
+import emailjs from "emailjs-com";
+import { getSelectedRoom } from "/server/functions/roomFunctions";
 
 export default {
   name: "Book",
-  email: "",
-  user: "",
-  password: "",
-  roomData: [],
   components: {
-    RoomCard
+    RoomCard,
   },
   props: ["roomId"],
   data() {
     return {
       valid: true,
-      user: '',
-      userRules: [
-        v => !!v || 'This field is required',
-      ],
-      password: '',
-      passwordRules: [
-        v => !!v || 'Password is required',
-        v => /^[a-z0-9]+$/.test(v) || 'Permitted only lowercase characters and numbers',
-        v => (v && v.length <= 8) || 'Password must be 8 characters/numbers',
-      ],
-      showPassword: false,
-
-      error:[]
-
+      email: "",
+      user: "",
+      password: "",
+      roomData: [],
+      error: [],
     };
-},
-created(){
-  let vm = this;
-  getSelectedRoom().then(function (data) {
+  },
+  created() {
+    let vm = this;
+    console.log(vm.roomId);
+    getSelectedRoom().then(function(data) {
       vm.roomData = data;
-      console.log("data", vm.roomData)
+      console.log("data", vm.roomData);
     });
-},
- methods:{
+  },
+  methods: {
     sendEmail(e) {
       try {
-        emailjs.sendForm('service_gqqo60m', 'template_ykmyzgj', e.target,
-        'user_OvCYTVO2E6wAnouSJYryv', {
-          email: this.email,
-          user: this.user,
-          password: this.password,
-        })
-        console.log("Success")
-      } catch(error) {
-          console.log("Failed", {error})
+        emailjs.sendForm(
+          "service_gqqo60m",
+          "template_ykmyzgj",
+          e.target,
+          "user_OvCYTVO2E6wAnouSJYryv",
+          {
+            email: this.email,
+            user: this.user,
+            password: this.password,
+          }
+        );
+        console.log("Success");
+      } catch (error) {
+        console.log("Failed", { error });
       }
     },
-  }
+  },
 };
-
-
-
 </script>
 
 <style scoped>
-
-
 .title {
-  font-family: 'Ruluko', sans-serif !important;
+  font-family: "Ruluko", sans-serif !important;
   font-size: 200% !important;
-  border-bottom: 2px solid #00BCD4;
+  border-bottom: 2px solid #00bcd4;
 }
-
 </style>
